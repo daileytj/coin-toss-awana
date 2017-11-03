68076df140ee82dced44ea810d75ab0fca1e2b7c
Thomas Dailey <daileytj89@gmail.com>
2017-11-03 16:42:52 -0400

    coin toss game update

---
+++ index.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+let coinFlipResult = "heads";
+
+
+
+const coinFlip = () => {
+  var coinResult = Math.floor(Math.random() * 2);
+  if (coinResult === 0) {
+    coinFlipResult = "Heads";
+  } else {
+    coinFlipResult = "Tails";
+  }
+};
+
+$(".flip").on('click', function() {
+  $(".resultText").html("");
+  coinFlip();
+  setTimeout(function(){
+    $(".resultText").html(coinFlipResult);
+},2000);
+  if (coinFlipResult === "Heads") {
+    $(".coin").css("background-image", "url(./heads.jpg)");
+    $('#coin').removeClass();
+    setTimeout(function() {
+      $('#coin').addClass('animation1800');
+    }, 100);
+    setTimeout(function(){
+      $("<audio></audio>").attr({
+  		'src':'pastorRonVictory.m4a',
+  		'volume':0.6,
+  		'autoplay':'autoplay'
+  	}).appendTo("body");
+    },2000);
+  } else {
+    $(".coin").css("background-image", "url(./tails.jpg)");
+    $('#coin').removeClass();
+    setTimeout(function() {
+      $('#coin').addClass('animation1620');
+    }, 100);
+  }
+});
+});
